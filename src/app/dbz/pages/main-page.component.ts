b2d4fec6749a839da1411d698399da9ba8a6bfda
Daniel_Cuellar <danielfelipecuellarrocha@gmail.com>
2023-09-12 22:21:24 -0500

    12-09

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, } from '@angular/core';
 import { Character } from '../interfaces/character.interfaces';
+import { dbzService } from '../services/dbz.service';
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -8,23 +9,5 @@ import { Character } from '../interfaces/character.interfaces';
 
 export class mainPage  {
 
-    public characters:Character[] = [{
-        name:"krilin",
-        power:1000
-    },{
-        name:"Goku",
-        power:10000
-
-}];
-
-onNewCharacter2(character:Character): void{
-    this.characters.push(character);
-    console.log(character)
-}
-
-onDeletePersonaje(index:number){
-    this.characters.splice(index,1)
-    console.log(this.characters)
-
-}    
+     constructor( public DBZService: dbzService){}
 }
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,37 @@
+
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from "uuid";
+
+import { Character } from '../interfaces/character.interfaces';
+
+@Injectable({providedIn: 'root'})
+export class dbzService {
+    public characters:Character[] = [{
+        id: uuid(),
+        name:"krilin",
+        power:1000
+    },{
+        id: uuid(),
+        name:"Goku",
+        power:10000
+
+}];
+
+onNewCharacter2(character:Character): void{
+    const newCharacter: Character={ id: uuid(), ...character};
+    this.characters.push(newCharacter);
+    console.log(newCharacter)
+}
+
+onDeletePersonaje(index:number){
+    this.characters.splice(index,1)
+    console.log(this.characters)
+
+}  
+
+deleteCharacterByID(id:string){
+    this.characters.filter
+}
+   
+    
+}
